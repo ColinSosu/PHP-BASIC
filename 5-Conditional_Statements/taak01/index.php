581c76d9cea938868f0e360fb1584382c3a44cd9
Colin <coli-nm@hotmail.com>
2020-05-25 23:00:57 +0200

    OPDRACHTEN

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$test1 = 12;
+$test2 = "gewoone tekst"
+
+var_dump($test1 == $test2);
+
+?>
---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$variabele1 = 10;
+$variabele2 = 10;
+
+
+if($variabele1 == $variabele2) {
+    echo "de vergelijking is waar!";
+}
+
+
+?>
