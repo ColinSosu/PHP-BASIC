97be5b089f4472a5ef3fd4511f70c1e2c77635f0
Colin <coli-nm@hotmail.com>
2020-05-28 03:11:47 +0200

    taak

---
+++ 5-Conditional_Statements/taak05/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$score = 6;
+
+switch($score) {
+    case 6:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 5:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 4:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 3:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+?>
